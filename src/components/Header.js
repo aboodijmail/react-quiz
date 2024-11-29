25cc4e88ac6d8be853cd1c9d636c1df21658c562
abed <aboodhih@yahoo.com>
2024-11-29 21:26:53 +0300

    a

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,11 @@
+
+
 function Header() {
   return (
     <header className='app-header'>
       <img src='logo512.png' alt='React logo' />
-      <h1>The React Quiz</h1>
+      <h1>The React Quiz </h1>
+      <span>by abed</span>
     </header>
   );
 }
